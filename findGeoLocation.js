9a8ae8c54219127a68fef03cb1ebcab6681579e9
EX32 <107504668+XEX32@users.noreply.github.com>
2022-10-05 12:56:25 -0400

    Update findGeoLocation.js

--- findGeoLocation.js
+++ findGeoLocation.js
@@ -1,17 +1,13 @@
-//first use of js on one of my ideas.
-
-
-
-// import readline module
 const readline = require("readline");
+// import readline module
 
-// create interface for input and output
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+// create interface for input and output
 
-// create empty user input
 let userInput = '';
 let bodyResponse = '';
 
@@ -19,28 +15,30 @@ function requestFunctionAndParse()
 {
     const request = require('request');
 
-    request('https://ipinfo.io/' + userInput.toString(), function (error, response, body) {
+    request('https://ipinfo.io/' + userInput.toString(), function (error, response, body) 
+    {
     
-    console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    bodyResponse = JSON.parse(body); // Parse JSON response.
+    console.error('error:', error);
+    // Print the error if one occurred
 
-    const obj = JSON.parse(body);
+    console.log('statusCode:', response && response.statusCode);
+    // Print the response status code if a response was received
+    bodyResponse = JSON.parse(body);
+    // Parse JSON response
 
-    console.log('IP Adress:', obj.ip);
-    console.log('City:', obj.city);
-    console.log('Region:', obj.region);
-    console.log('Country:', obj.country);
-    console.log('Location:', obj.loc);
-    console.log('ORG:', obj.org);
-    console.log('Postal:', obj.postal);
-    console.log('Timezone:', obj.timezone); // Print parsed the ipinfo.io JSON.
-    });
+    let obj = JSON.parse(body);
+
+    for (const [key, value] of Object.entries(obj)) 
+    {
+      console.log(`${key}: ${value}`);
+      // Loop through all keys and values in object and print them
+    }
+  });
 }
 
 rl.question("What is the IP?\n", function (string) {
     userInput = string;
-    console.log(requestFunctionAndParse());
+    requestFunctionAndParse();
     // close input stream
     rl.close();  
 });
